3e01f803124e9a1545c0229a3c14837fc4397d8c
hieutq <quanghieuit.hubt@gmail.com>
2017-04-18 17:36:03 +0700

    commit file testting

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Validator;
 class Member extends Model
 {
-    use SoftDeletes;
      protected $table = 'members';
      protected $fillable = ['name','gender','age','address','photo','status'];
 
