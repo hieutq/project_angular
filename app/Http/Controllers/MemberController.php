77e134277acc0526dfe9d2eb8cff5c61c40042b3
hieutq <quanghieuit.hubt@gmail.com>
2017-04-19 17:51:47 +0700

    fix all file

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -7,79 +7,74 @@ use App\Models\Member;
 use App\Http\Requests\MemberRequest;
 use Validator;
 use DB;
+
 class MemberController extends Controller
 {
-   public function getList() 
-   {
+    public function getList()
+    {
         $datas =  Member::all();
         return response()->json($datas);
     }
 
-    public function getAdd (Request $request) 
+    public function getAdd(Request $request)
     {
-
-        $data = Member::Validate_rule ($request->all(), Member::$rules, Member::$messages);
+        $data = Member::Validate_rule($request->all(), Member::$rules, Member::$messages);
 
         if ($data['error']) {
             return response()->json([
-                'error'      => true,
+                'error'      => false,
                 'messages'   =>$data['messages']
-            ],405);
-
+                ], 200);
         }
 
         DB::beginTransaction();
         try {
-
-            $imageName = time().'.'.$request->photo->getClientOriginalExtension();
-            $request->photo->move(public_path('images'), $imageName);
-            $data = $request->all();
-            $data['name']       = trim($request->name); 
-            $data['gender']     = trim($request->gender);
-            $data['age']        = trim($request->age);
-            $data['address']    = trim($request->address);
-            $data['photo']      = $imageName;
-            $datas = Member::create($data);
-            DB::commit();
-            return $this->getList();
-
+            if ($request->hasFile('photo')) {
+                $imageName = time().'.'.$request->photo->getClientOriginalExtension();
+                $request->photo->move(public_path('images'), $imageName);
+                $data = $request->all();
+                $data['name']       = trim($request->name);
+                $data['gender']     = trim($request->gender);
+                $data['age']        = trim($request->age);
+                $data['address']    = trim($request->address);
+                $data['photo']      = $imageName;
+                $datas = Member::create($data);
+                DB::commit();
+                return $this->getList();
+            }           
         } catch (Exception $e) {
-
             DB::rollback();
-
-        }   
+        }
     }
 
-    public function getEdit ($id) 
+    public function getEdit($id)
     {
-
         $id = $id;
         $data = Member::find($id);
         return response()->json($data);
-
     }
 
-    public function postEdit (Request $request ,$id) 
+    public function postEdit(Request $request, $id)
     {
         $requestAll = $request->all();
         $rules = [
-            'name'      => 'required|max:100',
-            'gender'    => 'required|numeric',
-            'age'       => 'required|numeric|digits:2',
-            'address'   => 'required|max:300',
+        'name'      => 'required|max:100',
+        'gender'    => 'required|numeric',
+        'age'       => 'required|numeric|digits:2',
+        'address'   => 'required|max:300',
 
         ];
 
         $messages = [
-            'name.required'         => 'Tell us your name.',
-            'name.max'              => 'your name wrong. max 100 string',
-            'gender.required'       => 'Tell us your gender.',
-            'gender.numeric'        => 'Your gender Your age must be a numberic',
-            'age.required'          => 'Tell us your age',
-            'age.numberic'          => 'Your age Your age must be a numberic',
-            'age.digits'            => 'Your age must be a a 2 digit number',
-            'address.max'           => 'The address may not be greater than 300 characters.',  
-            'address.required'      => 'Tell us your address', 
+        'name.required'         => 'Tell us your name.',
+        'name.max'              => 'your name wrong. max 100 string',
+        'gender.required'       => 'Tell us your gender.',
+        'gender.numeric'        => 'Your gender Your age must be a numberic',
+        'age.required'          => 'Tell us your age',
+        'age.numberic'          => 'Your age Your age must be a numberic',
+        'age.digits'            => 'Your age must be a a 2 digit number',
+        'address.max'           => 'The address may not be greater than 300 characters.',
+        'address.required'      => 'Tell us your address',
 
         ];
 
@@ -87,52 +82,43 @@ class MemberController extends Controller
         
         if ($validator->fails()) {
             return response()->json([
-                'error' => true,
+                'error' => false,
                 'messages' => $validator->errors(),
-            ], 405);
+                ], 200);
         }
 
         DB::beginTransaction();
         try {
-            if (!$request->hasFile('photo')) 
-            {
-
-
+            if (!$request->hasFile('photo')) {
                 $customer=Member::find($id);
 
-                $customer->name     = $request->name;
-                $customer->gender   = $request->gender;
-                $customer->age      = $request->age;
-                $customer->address  = $request->address;
+                $customer->name     = trim($request->name);
+                $customer->gender   = trim($request->gender);
+                $customer->age      = trim($request->age);
+                $customer->address  = trim($request->address);
                 $customer->save();
                 return $this->getList();
-                
-            }
-            else 
-            {
-                    $imageName = rand(1111,1000).'.'.$request->photo->getClientOriginalExtension();
-                    $request->photo->move(public_path('images'), $imageName);
-                    $id = $id;
-                    $customer = Member::find($id);
-                    $customer->name     = trim($request->name);
-                    $customer->gender   = trim($request->gender);
-                    $customer->age      = trim($request->age);
-                    $customer->address  = trim($request->address);
-                    $customer->photo    = $imageName; 
-                    $customer->save();
+            } else {
+                $imageName = rand(1111, 1000).'.'.$request->photo->getClientOriginalExtension();
+                $request->photo->move(public_path('images'), $imageName);
+                $id = $id;
+                $customer = Member::find($id);
+                $customer->name     = trim($request->name);
+                $customer->gender   = trim($request->gender);
+                $customer->age      = trim($request->age);
+                $customer->address  = trim($request->address);
+                $customer->photo    = $imageName;
+                $customer->save();
 
-                    DB::commit();
-                    return $this->getList();    
-                
-                
+                DB::commit();
+                return $this->getList();
             }
-        } 
-        catch (Exception $e) {
+        } catch (Exception $e) {
             DB::rollback();
         }
     }
 
-    public function deleteMember ($id) 
+    public function deleteMember($id)
     {
         $customer = Member::find($id);
         $customer->delete();
@@ -140,14 +126,13 @@ class MemberController extends Controller
     }
 
 
-    public function uploadImage (Request $request) 
+    public function uploadImage(Request $request)
     {
-        dd($request->photo);
         if ($request->hasFile('photo')) {
             $file       = $request->file('photo');
             $filename   = $file->getClientOriginalName();
             $image      = $file->getClientOriginalExtension();
-            $imageName  = rand(1111,10000).'-'.$filename;
+            $imageName  = rand(1111, 10000).'-'.$filename;
             $allowed    = array('jpg','png','gif');
             $path       = public_path('images');
             if (in_array($image, $allowed)) {
@@ -155,12 +140,12 @@ class MemberController extends Controller
                 return response()->json([
                     'error'     => true,
                     'pathFile'  => $imageName,
-                ]);
-            }else {
+                    ]);
+            } else {
                 return response()->json([
                     'error'     => false,
                     'messages'  => 'image format wrong!'
-                ]);
+                    ]);
             }
         }
     }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,35 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Validator;
+
 class Member extends Model
 {
-     protected $table = 'members';
-     protected $fillable = ['name','gender','age','address','photo','status'];
+    protected $table = 'members';
+    protected $fillable = ['name','gender','age','address','photo','status'];
 
 
     public static $rules = [
-        'name'      =>'required|max:100',
-        'gender'    =>'required|numeric',
-        'age'       =>'required|numeric|digits:2',
-        'photo'     =>'required|image|mimes:jpeg,png,gif|max:10240',
-        'address'   =>'required|max:300',
+    'name'      =>'required|max:100',
+    'gender'    =>'required|numeric',
+    'age'       =>'required|numeric|digits:2',
+    'photo'     =>'image|mimes:jpeg,png,gif|max:10240',
+    'address'   =>'required|max:300',
 
     ];
 
     public static $messages = [
-        'name.required'         => 'Tell us your name.',
-        'name.max'              => 'The name may not be greater than 100 characters.',
-        'gender.required'       => 'Tell us your gender.',
-        'gender.numeric'        => 'The gender must be a number.',
-        'age.required'          => 'Tell us your age',
-        'age.numberic'          => 'The age must be a number.',
-        'age.digits'            => 'The age must be 2 digits.',
-        'photo.required'        => 'Selected a image',
-        'photo.image'           => 'The photo must be an image.',
-        'photo.mimes'           => 'The photo must be a file of type: jpeg, png, gif.',
-        'photo.max'             => 'The photo may not be greater than 10 MB.',
-        'address.max'           => 'The address may not be greater than 300 characters.',  
-        'address.required'      => 'Tell us your address'                 
+    'name.required'         => 'Tell us your name.',
+    'name.max'              => 'The name may not be greater than 100 characters.',
+    'gender.required'       => 'Tell us your gender.',
+    'gender.numeric'        => 'The gender must be a number.',
+    'age.required'          => 'Tell us your age',
+    'age.numberic'          => 'The age must be a number.',
+    'age.digits'            => 'The age must be 2 digits.',
+        // 'photo.required'        => 'Selected a image',
+    'photo.image'           => 'The photo must be an image.',
+    'photo.mimes'           => 'The photo must be a file of type: jpeg, png, gif.',
+    'photo.max'             => 'The photo may not be greater than 10 MB.',
+    'address.max'           => 'The address may not be greater than 300 characters.',
+    'address.required'      => 'Tell us your address'
     ];
 
     /**
@@ -40,17 +41,18 @@ class Member extends Model
      *
      * @var array
      */
-    public static function Validate_rule($input, $rules, $messages) {
+    public static function Validate_rule($input, $rules, $messages)
+    {
         $validator =Validator::make($input, $rules, $messages);
         if ($validator->fails()) {
             return [
-                'error' => true,
-                'messages' => $validator->errors()
-           ];
+            'error' => true,
+            'messages' => $validator->errors()
+            ];
         }
         return [
-            'error' => false,
-            'messages' => 'successfully'
+        'error' => false,
+        'messages' => 'successfully'
         ];
     }
 }
