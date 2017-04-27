0834940969656e24a5ea3448951ff7f608693ec8
hieutq <quanghieuit.hubt@gmail.com>
2017-04-27 10:01:55 +0700

    fix all files in testting

--- tests/Feature/controller/AddValidationTest.php
+++ tests/Feature/controller/AddValidationTest.php
@@ -11,138 +11,160 @@ class AddValidationTest extends TestCase
 {
     use DatabaseMigrations;
     use WithoutMiddleware;
+
     /**
      * A basic test example.
      * @test
      * @return void
      */
-    protected function assertFalseState($request_array)
-    {
-        $response = $this->call('POST', '/add', $request_array);
-        $data = json_decode($response ->getContent(), true);
-        $this->assertEquals(200, $response->status());
-        if ($data['error']) {
-            $this->assertTrue(true);
-        } else  if ($data['error'] == false) {
-            print_r($data['messages']);
-            $this->assertTrue(true);
-        }
-    }
-
     /** Test validate name 101 is larger than characters */
 
-    public function testValidationName()
+    public function testAddName101Character()
     {
-       $request_array = [
-            'name'      => '<script>alert(ok)</script>',
-            'age'       => 22, 
-            'address'   => 'Vĩnh Phúc', 
-            'gender'    => 1, 
-            ];
-        $this->assertFalseState($request_array);
-    }
-    /** tests validation not required */
-    public function testIsNameEmpty()
-    {
-        $request_Member = [
-        'name'     => '',
-        'address'  => 'Test Đia Chi',
-        'age'      => 22,
-        'gender'   => 1,
+        $request_array = [
+            'name' => '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
+            'age' => 22,
+            'address' => 'Vĩnh Phúc',
+            'gender' => 1,
         ];
-        $this->assertFalseState($request_Member);
+        $response = $this->call('POST', '/add', $request_array);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(405, $response->status());
+        $this->assertDatabaseMissing('members',
+            ['name' => $request_array['name'], 'age' =>$request_array['age'], 'address' => $request_array['address'], 'gender' => $request_array['gender']]);
     }
-    /** test validate address larger than 300 character */
-    public function testAddressValidation()
+
+    /** tests validation not required */
+    public function testAddNameEmpty()
     {
-        $request_Member = [
-        'name'      => 'Tạ Quang Hiếu',
-        'address'   => 'addresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstest',
-        'age'       => 22,
-        'gender'    => 1,
+        $request_array = [
+            'name' => '',
+            'address' => 'Test Đia Chi',
+            'age' => 22,
+            'gender' => 1,
+        ];
+        $response = $this->call('POST', '/add', $request_array);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(405, $response->status());
+        $this->assertDatabaseMissing('members',
+            ['name' => $request_array['name'], 'age' =>$request_array['age'], 'address' => $request_array['address'], 'gender' => $request_array['gender']]);
+    }
+
+    /** test validate address larger than 300 character */
+    public function testAddAddress301character()
+    {
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'addresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstestaddresstest',
+            'age' => 22,
+            'gender' => 1,
 
         ];
-        $this->assertFalseState($request_Member);
+        $response = $this->call('POST', '/add', $request_array);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(405, $response->status());
+        $this->assertDatabaseMissing('members',
+            ['name' => $request_array['name'], 'age' =>$request_array['age'], 'address' => $request_array['address'], 'gender' => $request_array['gender']]);
     }
 
     /** test empty address */
 
     public function testIsAddressEmpty()
     {
-        $request_Member = [
-        'name'        => 'Tạ Quang Hiếu',
-        'address'     => '',
-        'age'         => 22,
-        'gender'      => 1,
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => '',
+            'age' => 22,
+            'gender' => 1,
 
         ];
 
-        $this->assertFalseState($request_Member);
+        $response = $this->call('POST', '/add', $request_array);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(405, $response->status());
+        $this->assertDatabaseMissing('members',
+            ['name' => $request_array['name'], 'age' =>$request_array['age'], 'address' => $request_array['address'], 'gender' => $request_array['gender']]);
     }
 
     /**
      * test column age have type more than 2 digits
      */
-    public function testAgeValidation()
+    public function testAddAgeWhenMore2Digits()
     {
-        $request_Member = [
-        'name'      => 'Tạ Quang Hiếu',
-        'address'   => 'Vĩnh Phúc',
-        'age'       => 222,
-        'gender'    => 1,
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Vĩnh Phúc',
+            'age' => 222,
+            'gender' => 1,
 
         ];
 
-        $this->assertFalseState($request_Member);
+        $response = $this->call('POST', '/add', $request_array);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(405, $response->status());
+        $this->assertDatabaseMissing('members',
+            ['name' => $request_array['name'], 'age' =>$request_array['age'], 'address' => $request_array['address'], 'gender' => $request_array['gender']]);
     }
 
     /**
      * test column age required
      */
-    public function testAgeEmpty()
+    public function testAddAgeEmpty()
     {
-        $request_Member = [
-        'name'      => 'Tạ Quang Hiếu',
-        'address'   => 'Address Test',
-        'age'       => '',
-        'gender'    => 1,
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Address Test',
+            'age' => '',
+            'gender' => 1,
 
         ];
 
-        $this->assertFalseState($request_Member);
+        $response = $this->call('POST', '/add', $request_array);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(405, $response->status());
+        $this->assertDatabaseMissing('members',
+            ['name' => $request_array['name'], 'age' =>$request_array['age'], 'address' => $request_array['address'], 'gender' => $request_array['gender']]);
     }
 
 
     /**
      * test column age must be number
      */
-    public function testAgeNumber()
+    public function testAddAgeMustNotBeNumber()
     {
-        $request_Member = [
-        'name'      => 'Tạ Quang Hiếu',
-        'address'   => 'Address Test',
-        'age'       => 'aa',
-        'gender'    => 1,
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Address Test',
+            'age' => 'aa',
+            'gender' => 1,
 
         ];
 
-        $this->assertFalseState($request_Member);
+        $response = $this->call('POST', '/add', $request_array);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(405, $response->status());
+        $this->assertDatabaseMissing('members',
+            ['name' => $request_array['name'], 'age' =>$request_array['age'], 'address' => $request_array['address'], 'gender' => $request_array['gender']]);
     }
 
     /**
      * [testGenderEmptyEdit the column Gender required]
      * @return [type] [description]
      */
-    public function testGenderEmpty()
+    public function testAddGenderEmpty()
     {
         $request_array = [
-        'name'     => 'Tạ Quang Hiếu',
-        'address'  => 'Vĩnh Phúc',
-        'age'      => '22',
-        'gender'   => '',
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Vĩnh Phúc',
+            'age' => '22',
+            'gender' => '',
 
         ];
 
-        $this->assertFalseState($request_array);
+       $response = $this->call('POST', '/add', $request_array);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(405, $response->status());
+        $this->assertDatabaseMissing('members',
+            ['name' => $request_array['name'], 'age' =>$request_array['age'], 'address' => $request_array['address'], 'gender' => $request_array['gender']]);
     }
 }
--- tests/Feature/controller/UpdateValidationTest.php
+++ tests/Feature/controller/UpdateValidationTest.php
@@ -11,26 +11,27 @@ use App\Models\Member;
 class UpdateValidationTest extends TestCase
 {
     use DatabaseMigrations;
+
     protected function assertDatabaseMissingStatus($request_array)
     {
         $Member = factory(Member::class)->create([
-            'name'       => 'Babylong',
-            'age'        => 24,
-            'address'    => 'abc',
-            'gender'     => 1,
-            ]);
-        $id_member= $Member->id;
-        $response = $this->call('POST', '/edit/'.$id_member, $request_array);
+            'name' => 'Babylong',
+            'age' => 24,
+            'address' => 'abc',
+            'gender' => 1,
+        ]);
+        $id_member = $Member->id;
+        $response = $this->call('POST', '/edit/' . $id_member, $request_array);
         // $this->assertDatabaseHas('members' ,['name' => $request_array['name'] ,'address' => $request_array['address'] ,'age' => (int)$request_array['age'] ,'gender' => (int)$request_array['gender']]);
         $data = json_decode($response->getContent(), true);
         $this->assertEquals(200, $response->status());
-        if ($data['error']==false) {
+        if ($data['error'] == false) {
             print_r($data['messages']);
             $this->assertTrue(true);
         } else {
             $this->assertTrue(false);
         }
-        
+
     }
 
     /**
@@ -39,11 +40,11 @@ class UpdateValidationTest extends TestCase
      */
     public function testValidationNameEdit()
     {
-        $request_array  = [
-        'name'    => '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
-        'address' => 'Test Đia Chi',
-        'age'     => 22,
-        'gender'  => 1,
+        $request_array = [
+            'name' => '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
+            'address' => 'Test Đia Chi',
+            'age' => 22,
+            'gender' => 1,
         ];
         $this->assertDatabaseMissingStatus($request_array);
     }
@@ -54,11 +55,11 @@ class UpdateValidationTest extends TestCase
      */
     public function testIsNameEmptyEdit()
     {
-        $request_array  = [
-        'name'       => '',
-        'address'    => 'Test Đia Chi',
-        'age'        => 22,
-        'gender'     => 1,
+        $request_array = [
+            'name' => '',
+            'address' => 'Test Đia Chi',
+            'age' => 22,
+            'gender' => 1,
 
         ];
         $this->assertDatabaseMissingStatus($request_array);
@@ -68,14 +69,14 @@ class UpdateValidationTest extends TestCase
      * [testAddressValidationEdit address larger 300 character]
      * @return [type] [description]
      */
-    
+
     public function testAddressValidationEdit()
     {
-        $request_array  = [
-        'name'        => 'Tạ Quang Hiếu',
-        'address'     => 'Test Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia Chi',
-        'age'         => 22,
-        'gender'      => 1,
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Test Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia ChiTest Đia Chi',
+            'age' => 22,
+            'gender' => 1,
 
         ];
         $this->assertDatabaseMissingStatus($request_array);
@@ -85,30 +86,31 @@ class UpdateValidationTest extends TestCase
      * [testIsAddressEmptyEdit address riquired]
      * @return [type] [description]
      */
-    
+
     public function testIsAddressEmptyEdit()
     {
-        $request_array  = [
-        'name'      => 'Tạ Quang Hiếu',
-        'address'   => '',
-        'age'       => 22,
-        'gender'    => 1,
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => '', 
+            'age' => 22,
+            'gender' => 1,
 
         ];
         $this->assertDatabaseMissingStatus($request_array);
     }
+
     /**
      * [testAgeValidationEdit Age column more than 2 digits]
      * @return [type] [description]
      */
-    
+
     public function testAgeValidationEdit()
     {
-        $request_array  = [
-        'name'       => 'Tạ Quang Hiếu',
-        'address'    => 'Vĩnh Phúc',
-        'age'        => 222,
-        'gender'     => 1,
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Vĩnh Phúc',
+            'age' => 222,
+            'gender' => 1,
 
         ];
         $this->assertDatabaseMissingStatus($request_array);
@@ -118,14 +120,14 @@ class UpdateValidationTest extends TestCase
      * [testAgeEmptyEdit column Age required]
      * @return [type] [description]
      */
-    
+
     public function testAgeEmptyEdit()
     {
-        $request_array  = [
-        'name'        => 'Tạ Quang Hiếu',
-        'address'     => 'Vĩnh Phúc',
-        'age'         => '',
-        'gender'      => 1,
+        $request_array = [
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Vĩnh Phúc',
+            'age' => '',
+            'gender' => 1,
 
         ];
         $this->assertDatabaseMissingStatus($request_array);
@@ -135,14 +137,14 @@ class UpdateValidationTest extends TestCase
      * [testAgeNumberEdit The age mustn't be a number.]
      * @return [type] [description]
      */
-    
+
     public function testAgeNumberEdit()
     {
         $request_array = [
-        'name'        => 'Tạ Quang Hiếu',
-        'address'     => 'Vĩnh Phúc',
-        'age'         => 'aaaa',
-        'gender'      => 1,
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Vĩnh Phúc',
+            'age' => 'aaaa',
+            'gender' => 1,
 
         ];
         $this->assertDatabaseMissingStatus($request_array);
@@ -152,14 +154,14 @@ class UpdateValidationTest extends TestCase
      * [testGenderEmptyEdit the column Gender required]
      * @return [type] [description]
      */
-    
+
     public function testGenderEmptyEdit()
     {
         $request_array = [
-        'name'        => 'Tạ Quang Hiếu',
-        'address'     => 'Vĩnh Phúc',
-        'age'         => '22',
-        'gender'      => '',
+            'name' => 'Tạ Quang Hiếu',
+            'address' => 'Vĩnh Phúc',
+            'age' => '22',
+            'gender' => '',
 
         ];
         $this->assertDatabaseMissingStatus($request_array);
